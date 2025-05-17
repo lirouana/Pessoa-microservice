0051adf65a9bbe8798003c779764ecc4bb4a03fd
João Pedro Silva Antunes <antunesjoaopedro3@gmail.com>
2025-05-17 10:35:16 -0300

    adicionando banco de dados SQL Lite

--- app.py
+++ app.py
@@ -4,5 +4,10 @@ from controllers.pessoa_controller import pessoa_bp
 app = create_app()
 app.register_blueprint(pessoa_bp, url_prefix='/pessoas')
 
+from models.BancoSQLite import inicializar_banco
+
+# Inicializa o banco de dados
+inicializar_banco()
+
 if __name__ == '__main__':
     app.run(host='localhost', port=5001)
